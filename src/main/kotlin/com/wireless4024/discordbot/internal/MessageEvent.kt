b685c7cebb2f9450457c6727d7c456081c1b620a
Wittawat Manha <36229606+Wireless4024@users.noreply.github.com>
2019-09-02 13:26:49 +0700

    extended message received event

--- src/main/kotlin/com/wireless4024/discordbot/internal/MessageEvent.kt
+++ src/main/kotlin/com/wireless4024/discordbot/internal/MessageEvent.kt
@@ -1,7 +1,10 @@
 package com.wireless4024.discordbot.internal
 
 import com.wireless4024.discordbot.internal.Property.Companion.Permission
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import java.util.concurrent.TimeUnit
 
 open class MessageEvent(private val e: MessageReceivedEvent?) {
 	val ev
@@ -15,23 +18,61 @@ open class MessageEvent(private val e: MessageReceivedEvent?) {
 	val txtch
 		get() = this.e!!.textChannel
 	var reply: Any? = null
-		set(text) = this.reply(text)
+		set(text) = this.reply(text, deep = 1)
+	var permreply: Any? = null
+		set(text) = this.reply(text, true)
+	open val msg = Utils.getParameter(this.e!!.message.contentDisplay.trim('`', ' '))
+	val guild
+		get() = this.e!!.guild
+	val configuration
+		get() = ConfigurationCache.get(guild)
+	val voiceChannel
+		get() = member.voiceState?.channel
+	val musicController = configuration.musicController
 
-	open fun reply(text: Any?) {
-		this.ch.send(text)
+	open fun reply(text: Any?, permanent: Boolean = false, deep: Int = 0) {
+		runBlocking {
+			launch {
+				Utils.log("-> [reply]\t'$text'", deep = 11 + deep)
+				ch.send(text) {
+					if (!permanent) {
+						TimeUnit.SECONDS.sleep(Property.BASE_SLEEP_DELAY)
+						Utils.log("-> [delete]\t'${it.contentDisplay}'")
+						it.delete().queue()
+					}
+				}
+			}
+		}
 	}
 
 	open fun chperm(permission: Int): Boolean {
 		return Permission.check(member, permission)
 	}
+
+	fun <T> ensureVoiceConnected(then: (() -> T)): T {
+		if (member.voiceState?.channel == null)
+			throw CommandError("You must be in voice channel to use command")
+		return then.invoke()
+	}
+
+	fun <T> ensureVoiceConnected(then: T): T {
+		if (member.voiceState?.channel == null)
+			throw CommandError("You must be in voice channel to use command")
+		return then
+	}
+
+	fun ensureVoiceConnected() {
+		if (member.voiceState?.channel == null)
+			throw CommandError("You must be in voice channel to use command")
+	}
 }
 
-class ConsoleEvent : MessageEvent(null) {
-	override fun reply(text: Any?) {
+class ConsoleEvent(private val message: String) : MessageEvent(null) {
+	override val msg: String = this.message
+
+	override fun reply(text: Any?, permanent: Boolean, deep: Int) {
 		if (text != null && text.toString().isNotBlank()) print(text)
 	}
 
-	override fun chperm(permission: Int): Boolean {
-		return true
-	}
+	override fun chperm(permission: Int): Boolean = true
 }
