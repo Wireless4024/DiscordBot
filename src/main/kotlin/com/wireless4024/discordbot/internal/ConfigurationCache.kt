75eafb6ff58218c58b7a89a23ab299d709acc6de
Wittawat Manha <36229606+Wireless4024@users.noreply.github.com>
2019-09-02 17:31:52 +0700

    implement cached setting

--- src/main/kotlin/com/wireless4024/discordbot/internal/ConfigurationCache.kt
+++ src/main/kotlin/com/wireless4024/discordbot/internal/ConfigurationCache.kt
@@ -1,7 +1,11 @@
 package com.wireless4024.discordbot.internal
 
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers
+import com.wireless4024.discordbot.internal.music.Controller
 import net.dv8tion.jda.api.Region
 import net.dv8tion.jda.api.Region.UNKNOWN
+import net.dv8tion.jda.api.audio.AudioSendHandler
 import net.dv8tion.jda.api.entities.Guild
 
 class ConfigurationCache private constructor(var guild: Guild) {
@@ -9,19 +13,28 @@ class ConfigurationCache private constructor(var guild: Guild) {
 		private val Cache = mutableMapOf<Long, ConfigurationCache>()
 
 		fun get(guild: Guild): ConfigurationCache {
-			if (Cache.containsKey(guild.idLong))
+			if (!Cache.containsKey(guild.idLong))
 				Cache[guild.idLong] = ConfigurationCache(guild)
 			return Cache[guild.idLong]!!.update(guild)
 		}
 	}
+
+	val audioPlayerManager = DefaultAudioPlayerManager().also { AudioSourceManagers.registerRemoteSources(it) }
+
+	val musicController = Controller(this)
+
+	var audioSendHandler: AudioSendHandler?
+		get() = audioManager.sendingHandler
+		set(value) = with(value) { audioManager.sendingHandler = this }
 
 	val audioManager
 		get() = guild.audioManager
 
 	fun closeAudioConnection() = audioManager.closeAudioConnection()
 
-	fun update(guild: Guild): ConfigurationCache = this.also {
-		this.guild = guild
+	fun update(guild: Guild? = null): ConfigurationCache = this.also {
+		if (guild != null)
+			this.guild = guild
 	}
 
 	fun ban(id: String, delay: Int = 0, reason: String = "") {
