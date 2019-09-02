b74d137d750f338f5aacc59b7dae23f7d9cfa967
Wittawat Manha <36229606+Wireless4024@users.noreply.github.com>
2019-09-02 13:17:04 +0700

    commit change

--- src/main/kotlin/com/wireless4024/discordbot/Main.kt
+++ src/main/kotlin/com/wireless4024/discordbot/Main.kt
@@ -1,11 +1,17 @@
 package com.wireless4024.discordbot
 
-import com.wireless4024.discordbot.command.kts
+import com.sedmelluq.discord.lavaplayer.jdaudp.NativeAudioSendFactory
 import com.wireless4024.discordbot.internal.Handler
 import com.wireless4024.discordbot.internal.Property
+import net.dv8tion.jda.api.AccountType
 import net.dv8tion.jda.api.JDABuilder
 
 fun main() {
-	// JDABuilder(Property.TOKEN).addEventListeners(Handler()).build()
-	kts.callme()
+	System.setProperty("idea.io.use.fallback", "true")
+
+	JDABuilder(AccountType.BOT)
+			.setToken(Property.TOKEN)
+			.setAudioSendFactory(NativeAudioSendFactory())
+			.addEventListeners(Handler())
+			.build()
 }
