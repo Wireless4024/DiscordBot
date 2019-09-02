e436c032248f75cbab90ea7e8c8e47f00bd3f29a
Wittawat Manha <36229606+Wireless4024@users.noreply.github.com>
2019-09-02 13:08:57 +0700

    implement exit

---
+++ src/main/kotlin/com/wireless4024/discordbot/command/exit.kt
@@ -0,0 +1,17 @@
+package com.wireless4024.discordbot.command
+
+import com.wireless4024.discordbot.internal.ICommandBase
+import com.wireless4024.discordbot.internal.MessageEvent
+import com.wireless4024.discordbot.internal.Property
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.Option
+import kotlin.system.exitProcess
+
+class exit : ICommandBase {
+	override fun invoke(args: CommandLine, event: MessageEvent): Nothing {
+		exitProcess(0)
+	}
+
+	override val options: List<Option> = listOf()
+	override val permission: Int = Property.Companion.Permission.DEV
+}
