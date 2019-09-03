a51446bb44810e2fb3e3d153b9f101e198b1d663
Wittawat Manha <36229606+Wireless4024@users.noreply.github.com>
2019-09-03 21:53:06 +0700

    fix wrong condition

--- src/main/kotlin/com/wireless4024/discordbot/internal/music/Scheduler.kt
+++ src/main/kotlin/com/wireless4024/discordbot/internal/music/Scheduler.kt
@@ -78,7 +78,7 @@ class Scheduler(private val player: AudioPlayer,
 				player.stopTrack()
 				GlobalScope.launch {
 					delay(30000)
-					if (queue.isNotEmpty() && queue.first == null)
+					if (queue.isEmpty() || queue.first == null)
 						parent.leave()
 				}
 				// messageDispatcher.sendMessage("Queue finished.")
