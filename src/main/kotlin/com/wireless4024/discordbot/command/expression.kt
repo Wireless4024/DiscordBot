8530e68501da4fb4295852e31ca719bc001be64e
Wittawat Manha <36229606+Wireless4024@users.noreply.github.com>
2019-09-12 07:59:56 +0700

    allow server admin to edit Expression configuration

---
+++ src/main/kotlin/com/wireless4024/discordbot/command/expression.kt
@@ -0,0 +1,39 @@
+package com.wireless4024.discordbot.command
+
+import com.keelar.exprk.Expressions
+import com.wireless4024.discordbot.internal.CommandError
+import com.wireless4024.discordbot.internal.ICommandBase
+import com.wireless4024.discordbot.internal.MessageEvent
+import com.wireless4024.discordbot.internal.Property.Companion.Permission
+import com.wireless4024.discordbot.internal.get
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.Option
+import java.math.RoundingMode.CEILING
+import java.math.RoundingMode.FLOOR
+
+class expression : ICommandBase {
+	override fun invoke(args: CommandLine, event: MessageEvent): Any {
+		return when (args[0] ?: throw CommandError("Missing argument")) {
+			"rounding"  -> {
+				when (args[1] ?: "now") {
+					"floor" -> "changed rounding mode to ${event.configuration.Expressions.setRoundingMode(FLOOR).roundingMode.name}"
+					"ceil"  -> "changed rounding mode to ${event.configuration.Expressions.setRoundingMode(CEILING).roundingMode.name}"
+					"now"   -> event.configuration.Expressions.roundingMode.name
+					else    -> "[floor, ceil]"
+				}
+			}
+			"precision" -> {
+				event.configuration.Expressions.setPrecision(args[1]?.toIntOrNull() ?: -1)
+				"precision is ${event.configuration.Expressions.precision}"
+			}
+			"reset"     -> {
+				event.configuration.Expressions = Expressions()
+				"reset expression to default"
+			}
+			else        -> "unknown argument"
+		}
+	}
+
+	override val options: List<Option> = listOf()
+	override val permission: Int = Permission.ADMINISTRATOR
+}
