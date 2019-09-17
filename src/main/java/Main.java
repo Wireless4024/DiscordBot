95acb3ed5f3d33b409834b6077c189e9952d15da
Wittawat Manha <36229606+Wireless4024@users.noreply.github.com>
2019-09-17 22:40:56 +0700

    add main class for build jar with IntelliJ

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.sedmelluq.discord.lavaplayer.jdaudp.NativeAudioSendFactory;
+import com.wireless4024.discordbot.internal.Property;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		System.setProperty("idea.io.use.fallback", "true");
+
+		new JDABuilder(AccountType.BOT).setToken(Property.getTOKEN()).setAudioSendFactory(new NativeAudioSendFactory())
+		                               .addEventListeners(new com.wireless4024.discordbot.internal.Handler()).build();
+	}
+}
