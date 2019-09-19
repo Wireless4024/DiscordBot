0e911ba6a1faaa576bbbbaa29bd3b8c0f88e743f
Wittawat Manha <36229606+Wireless4024@users.noreply.github.com>
2019-09-19 16:20:44 +0700

    store event listener as single object

--- src/main/kotlin/com/wireless4024/discordbot/internal/Utils.kt
+++ src/main/kotlin/com/wireless4024/discordbot/internal/Utils.kt
@@ -26,6 +26,10 @@ class Utils {
 		val URL_Regex = Pattern.compile(
 				"^(http://www\\.|https://www\\.|http://|https://)?[a-z0-9]+([\\-.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\$"
 		)
+
+		@JvmStatic
+		val globalEvent = Handler()
+
 		@JvmStatic
 		val HAS_HTTP = Pattern.compile("^http")
 
