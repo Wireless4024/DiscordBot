51cbb0bad59741caee8d01a22232e3d9f76ba621
Wittawat Manha <36229606+Wireless4024@users.noreply.github.com>
2019-09-19 17:36:55 +0700

    add command now and baseboost;remove unused parameter

--- src/main/kotlin/com/wireless4024/discordbot/command/music.kt
+++ src/main/kotlin/com/wireless4024/discordbot/command/music.kt
@@ -10,33 +10,34 @@ class music : ICommandBase {
 	override fun invoke(args: CommandLine, event: MessageEvent): Any {
 		return when (args[0]) {
 			"play", "p"      -> p(args.dropFirst(), event)
-			"join", "j"      -> j(args.dropFirst(), event)
-			"leave", "l"     -> leave(args.dropFirst(), event)
-			"skip", "s"      -> s(args.dropFirst(), event)
+			"join", "j"      -> j(event)
+			"leave", "l"     -> leave(event)
+			"skip", "s"      -> s(event)
 			"vol", "v"       -> v(args.dropFirst(), event)
 			"queue", "q"     -> queue(args.dropFirst(), event)
-			"clear", "c"     -> clear(args.dropFirst(), event)
-			"pause"          -> pause(args.dropFirst(), event)
-			"repeat", "r"    -> repeat(args.dropFirst(), event)
+			"clear", "c"     -> clear(event)
+			"pause"          -> pause(event)
+			"repeat", "r"    -> repeat(event)
 			"remove", "d"    -> remove(args.dropFirst(), event)
-			"previous", "pv" -> previous(args.dropFirst(), event)
+			"previous", "pv" -> previous(event)
 			"forward", "fw"  -> forward(args.dropFirst(), event)
 			"backward", "bw" -> backward(args.dropFirst(), event)
 			"seek"           -> seek(args.dropFirst(), event)
+			"now"            -> now(event)
 			else             -> ""
 		}
 	}
 
 	@Command
 	fun p(args: CommandLine, event: MessageEvent): String =
-			event.ensureVoiceConnected { event.musicController.queue(args, event);"" }
+		event.ensureVoiceConnected { event.musicController.queue(args, event);"" }
 
 	@Command
-	fun j(args: CommandLine, event: MessageEvent): String =
-			event.ensureVoiceConnected { "connecting to '${event.musicController.join(event)}'" }
+	fun j(event: MessageEvent): String =
+		event.ensureVoiceConnected { "connecting to '${event.musicController.join(event)}'" }
 
 	@Command
-	fun s(args: CommandLine, event: MessageEvent): String {
+	fun s(event: MessageEvent): String {
 		event.ensureVoiceConnected()
 		val playing = event.musicController.skip(event)
 		return if (playing != null) "now playing $playing" else "player stopped"
@@ -49,13 +50,30 @@ class music : ICommandBase {
 	}
 
 	@Command
-	fun leave(args: CommandLine, event: MessageEvent): String {
+	fun now(event: MessageEvent): String {
+		val player = event.musicController.player().playingTrack ?: return "not playing"
+		val playingInfo = player.info
+		return """now playing: `${playingInfo.title}` 
+url: ${playingInfo.uri} 
+duration: `${Utils.toReadableFormatTime(playingInfo.length)}`
+played: `${Utils.toReadableFormatTime(player.position)}`
+remaining: `${Utils.toReadableFormatTime(playingInfo.length - player.position)}`"""
+	}
+
+	@Command
+	fun baseboost(event: MessageEvent): String {
+		event.ensureVoiceConnected()
+		return "base boost : ${if (event.musicController.baseBoost()) "on" else "off"}"
+	}
+
+	@Command
+	fun leave(event: MessageEvent): String {
 		event.ensureVoiceConnected()
 		return "I'm leaving ${event.musicController.leave(event)}"
 	}
 
 	@Command
-	fun clear(args: CommandLine, event: MessageEvent): String {
+	fun clear(event: MessageEvent): String {
 		event.musicController.clear()
 		return "cleared playlist"
 	}
@@ -63,47 +81,61 @@ class music : ICommandBase {
 	@Command
 	fun queue(args: CommandLine, event: MessageEvent): MessageEmbed {
 		return event.musicController.listAsEmbed(
-				event, args.args.getOrNull(0)?.toIntOrNull() ?: 1
+			event, args.args.getOrNull(0)?.toIntOrNull() ?: 1
 		)
 	}
 
 	@Command
-	fun pause(args: CommandLine, event: MessageEvent): String {
+	fun pause(event: MessageEvent): String {
 		return if (event.musicController.pause()) "paused " else "resume"
 	}
 
 	@Command
-	fun repeat(args: CommandLine, event: MessageEvent): String {
+	fun repeat(event: MessageEvent): String {
 		return if (event.musicController.repeat()) "repeat : on" else "repeat : off"
 	}
 
 	@Command
 	fun remove(args: CommandLine, event: MessageEvent): String {
-		return "removed track : ${event.musicController.removeQueue(args.args.getOrNull(0)?.toIntOrNull()
-		                                                            ?: throw CommandError("invalid index")).info.title}"
+		return "removed track : ${event.musicController.removeQueue(
+			args.args.getOrNull(0)?.toIntOrNull()
+				?: throw CommandError("invalid index")
+		).info.title}"
 	}
 
 	@Command
-	fun previous(args: CommandLine, event: MessageEvent): String {
+	fun previous(event: MessageEvent): String {
 		return event.musicController.previous()?.let { "now playing : $it" } ?: ""
 	}
 
 	@Command
 	fun forward(args: CommandLine, event: MessageEvent): String {
-		return "now playing at ${Utils.toReadableFormatTime(event.musicController.forward((args[0]?.toIntOrNull()
-		                                                                                   ?: 0) * 1000))}"
+		return "now playing at ${Utils.toReadableFormatTime(
+			event.musicController.forward(
+				(args[0]?.toIntOrNull()
+					?: 0) * 1000
+			)
+		)}"
 	}
 
 	@Command
 	fun backward(args: CommandLine, event: MessageEvent): String {
-		return "now playing at ${Utils.toReadableFormatTime(event.musicController.back((args[0]?.toIntOrNull()
-		                                                                                ?: 0) * 1000))}"
+		return "now playing at ${Utils.toReadableFormatTime(
+			event.musicController.back(
+				(args[0]?.toIntOrNull()
+					?: 0) * 1000
+			)
+		)}"
 	}
 
 	@Command
 	fun seek(args: CommandLine, event: MessageEvent): String {
-		return "now playing at ${Utils.toReadableFormatTime(event.musicController.seek((args[0]?.toLongOrNull()
-		                                                                                ?: 0) * 1000))}"
+		return "now playing at ${Utils.toReadableFormatTime(
+			event.musicController.seek(
+				(args[0]?.toLongOrNull()
+					?: 0) * 1000
+			)
+		)}"
 	}
 
 	override val options: List<Option> = listOf()
