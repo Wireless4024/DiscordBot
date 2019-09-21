6820d7c9c36ad6a7508754c53549a93a231ca9c0
Wittawat Manha <36229606+Wireless4024@users.noreply.github.com>
2019-09-21 11:32:45 +0700

    implement random string function

---
+++ src/main/kotlin/com/wireless4024/discordbot/command/string/randstr.kt
@@ -0,0 +1,34 @@
+package com.wireless4024.discordbot.command.string
+
+import com.wireless4024.discordbot.internal.CommandError
+import com.wireless4024.discordbot.internal.ICommandBase
+import com.wireless4024.discordbot.internal.MessageEvent
+import com.wireless4024.discordbot.internal.Property.Companion.Permission
+import com.wireless4024.discordbot.internal.parseInt
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.Option
+
+class randstr : ICommandBase {
+	companion object {
+		@JvmStatic
+		val endWithNum = Regex("(?: )\\d+\$")
+	}
+
+	override fun invoke(args: CommandLine, event: MessageEvent): Any {
+		if (args.args.isEmpty())
+			throw CommandError("use case randstr <charlist> [limit]")
+		val charlist = event.msg
+		val charlen = charlist.length
+		val limit = endWithNum.find(charlist)?.value?.trim()?.parseInt() ?: charlen
+		val rnd = java.util.Random()
+		if (limit == charlen)
+			return String(charlist.toCharArray().toMutableList().also { it.shuffle(rnd) }.toCharArray())
+		val chars = CharArray(limit)
+		for (i in chars.indices)
+			chars[i] = charlist[rnd.nextInt(limit)]
+		return String(chars)
+	}
+
+	override val options: List<Option> = listOf()
+	override val permission: Int = Permission.ANY
+}
