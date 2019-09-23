f1b2d8b9e405e8f9ee115a85dfb044495b890261
Wittawat Manha <36229606+Wireless4024@users.noreply.github.com>
2019-09-23 15:07:38 +0700

    close System.out/err if has argument silent

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
-import com.sedmelluq.discord.lavaplayer.jdaudp.NativeAudioSendFactory;
 import com.wireless4024.discordbot.internal.Property;
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
+		if (args.length > 0 && args[0].equals("silent")) {
+			System.out.close(); System.err.close();
+		}
+
 		System.setProperty("idea.io.use.fallback", "true");
 
 		new JDABuilder(AccountType.BOT).setToken(Property.getTOKEN())//.setAudioSendFactory(new NativeAudioSendFactory())
