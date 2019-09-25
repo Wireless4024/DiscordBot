0642c43f2120e2fc748a8a52eafd0b88460c24e9
Wittawat Manha <36229606+Wireless4024@users.noreply.github.com>
2019-09-25 15:07:23 +0700

    fix no protocol if missing http(s)://

--- src/main/kotlin/com/wireless4024/discordbot/command/network/header.kt
+++ src/main/kotlin/com/wireless4024/discordbot/command/network/header.kt
@@ -3,19 +3,23 @@ package com.wireless4024.discordbot.command.network
 import com.wireless4024.discordbot.internal.ICommandBase
 import com.wireless4024.discordbot.internal.MessageEvent
 import com.wireless4024.discordbot.internal.Property.Companion.Permission
+import com.wireless4024.discordbot.internal.SkipArguments
+import com.wireless4024.discordbot.internal.Utils
 import org.apache.commons.cli.CommandLine
 import org.apache.commons.cli.Option
 import java.net.URL
 import java.net.URLConnection
 
+@SkipArguments
 class header : ICommandBase {
+
 	override fun invoke(args: CommandLine, event: MessageEvent): Any {
 		if (event.msg.isEmpty())
 			return "use case header <url>"
-		val obj = URL(event.msg)
+		val obj = URL(Utils.getFinalURL(event.msg))
 		val conn: URLConnection = obj.openConnection()
 		val headers = StringBuilder()
-		conn.headerFields.forEach { (key, value) -> headers.append(key).append("=").append(value[0]).append('\n') }
+		conn.headerFields.forEach { (key, value) -> headers.append(key).append("=").append(value).append('\n') }
 		return headers
 	}
 
